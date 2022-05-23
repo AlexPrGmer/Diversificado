a11cbf30a3492bd461fe4246dbba1b71abef0d36
AlexPrGmer <99362065+AlexPrGmer@users.noreply.github.com>
2022-05-23 13:30:11 -0600

    Commit_Examen

---
+++ DATABASES_My_SQL/Examen_final_2do_Bimestre.sql
@@ -0,0 +1,23 @@
+Create database Examen;
+
+Use Examen;
+
+create table FINAL (
+Id_1 integer auto_increment primary key,
+Nombre varchar (20),
+Apellido varchar (20),
+nota integer
+);
+
+Insert into Final(Nombre, Apellido, nota)
+values ('Gerber', 'Choc', 19 );
+
+Insert into Final(Nombre, Apellido, nota)
+values ('Emmanuel', 'Solorzano', 12 );
+
+Insert into Final(Nombre, Apellido, nota)
+values ('Henry', 'Xico', 8 );
+
+Select * from Final;
+
+
