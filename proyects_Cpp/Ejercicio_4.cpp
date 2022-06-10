57624670b9ceb3ffc2eb843f7aeb722e9ae9eace
AlexPrGmer <99362065+AlexPrGmer@users.noreply.github.com>
2022-06-10 13:27:20 -0600

    Add files via upload

---
+++ proyects_Cpp/Ejercicio_4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int myarray[3][3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    for (int i=0;i<3; i++){
+        for ( int j=0; j<3; j++)
+            for (int k=0; k<3; k++){
+
+                cout << myarray[i][j][k] << endl;
+            }
+    }
+    return 0;
+}
