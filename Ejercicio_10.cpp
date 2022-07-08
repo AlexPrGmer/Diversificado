a16629495d1dcec46be0d576b0d79febbf289f82
AlexPrGmer <99362065+AlexPrGmer@users.noreply.github.com>
2022-07-08 17:22:16 -0600

    Ejercicio_10

---
+++ Ejercicio_10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int num1 = 0, num2 = 0, num3 = 0;
+    cout<< "Ingrese el primer numero: ";
+    cin>> num1;
+    cout<< "Ingrese el segundo numero: ";
+    cin>> num2;
+    cout<< "Ingrese el tercer numero: ";
+    cin>> num3;
+
+    //
+    if (num1 < num2) {
+        cout<<"El primer numero es el mas pequeño" << endl;
+         }if (num2 < num3) {
+        cout<< "El segundo numero es el numero mediano" << endl;
+        cout<< "El Tercer numero es el mas grande" << endl;  
+
+        //       
+        //}if (num1 > num2){
+        //}if (num1 > num3){
+        //cout<< "El primer numero es el mas grande " << endl;
+        //}if (num2 < num3){
+          //  cout<<"El tercer numero es el mediano" << endl;
+           // cout<<"El segundo numero es el mas pequeño" << endl;
+    
+    //
+    //}if (num1 < num3) {
+    //}if (num2 > num3) {
+        //cout<< "El segundo numero es el mas grande" << endl;
+        // }if (num1 < num2) {
+      //  cout<< "El primer numero es el numero mas pequeño" << endl;
+        //cout<< "El tercer numero es el mediano" << endl;
+     }
+    }
