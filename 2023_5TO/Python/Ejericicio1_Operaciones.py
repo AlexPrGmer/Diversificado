c583c7c4c1d38d5cf6e8511b3ce33f8af0e62528
AlexPrGmer <99362065+AlexPrGmer@users.noreply.github.com>
2023-01-18 23:17:04 -0600

    Pyrhon

---
+++ 2023_5TO/Python/Ejericicio1_Operaciones.py
@@ -0,0 +1,25 @@
+print('ingresa tu nombre')
+nom = input()
+print(nom,'ingresa un numero entero')
+ent = input()
+print(nom,"ingresa un numero decimal")
+dec = input()
+print(nom,"ingresa una palabra")
+palabra = input()
+print(ent)
+print(dec)
+print(palabra)
+print(" ")
+
+
+n1 = float(input('por favor ingrese un numero: '))
+n2 = float(input('por favor ingrese otro numero: '))
+
+
+print("el resultado de la suma es", n1 + n2)
+print("el resultado de la resta es", n1-n2)
+print("el resultado de la multiplicacion es", n1*n2)
+print("el resultado de la division es", n1/n2)
+print("el resultado de la division es", n1/n2)
+print("el resultado de la potencia es", n1**n2)
+print("el resultado raiz es", (n1**(1/2)))
