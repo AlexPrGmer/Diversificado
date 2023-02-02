5c5e8181a8d005fbf5ee14215199b08211f3e7b8
AlexPrGmer <99362065+AlexPrGmer@users.noreply.github.com>
2023-02-02 10:11:52 -0600

    Ejemplo_1

---
+++ 2023_5TO/Python/Ejemplo_1.py
@@ -0,0 +1,46 @@
+numero_hexadecimal = 0x123f
+numero_binario = 0b1001
+numero_octal =0o123
+print(numero_hexadecimal)
+print (numero_binario)
+print(numero_octal)
+
+nombre_cadena = 'hola que hace'
+print(type(nombre_cadena))
+
+numero1 = 60
+print(type(numero1))
+
+numero2 = 3.1416
+print(type(numero2))
+
+tabla = ['futbol, basquetball, tenis']
+print(type(tabla))
+
+tupla = ("Gerber", "Luis", "Fernadndo")
+print(type(tupla))
+
+diccionario = {
+    'nombre':"Alex",
+    'edad':16
+}
+print(type(diccionario))
+
+var_1 = float(input())
+print(var_1)
+var_2 = bin(10)
+print(var_2)
+var_3 = hex(15)
+print(var_3)
+
+#--------------estructura de control-------------------------
+a = 0
+if a > 9:
+    print(a*4)
+
+x = 5
+if x == 5:
+    print("Es 5")
+else:
+    print("No es 5")
+
